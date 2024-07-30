e9b2b472b16b525d2e8e49fd1762562de105b35f
0sei531 <osekofiantwi531@gmail.com>
2024-07-30 18:22:44 +0000

    lyn

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """ Route module for the API - Basic Babel setup """
 
-
 from flask import Flask, request, render_template
 from flask_babel import Babel
 from os import getenv
@@ -9,26 +8,22 @@ from os import getenv
 app = Flask(__name__)
 babel = Babel(app)
 
-
 class Config(object):
     """ Setup - Babel configuration """
-    LANGUAGES = ['en', 'fr']
+    LANGUAGES = ["en", "fr"]
     # these are the inherent defaults just btw
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 # set the above class object as the configuration for the app
-app.config.from_object('1-app.Config')
+app.config.from_object("1-app.Config")
 
-
-@app.route('/', methods=['GET'], strict_slashes=False)
+@app.route("/", methods=["GET"], strict_slashes=False)
 def index() -> str:
     """ GET /
     Return: 1-index.html
     """
-    return render_template('1-index.html')
-
+    return render_template("1-index.html")
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")

fb30f4b75adb497c3b910aecacde7054b5e88224
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-07-30 05:17:11 -1200

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,10 +4,12 @@
 from flask import Flask, request, render_template
 from os import getenv
 
+
 class Config:
     """Configuration class for Flask app"""
     API_HOST = getenv("API_HOST", "0.0.0.0")
     API_PORT = getenv("API_PORT", 5000)
+
 
 app = Flask(__name__, static_url_path='')
 app.config.from_object(Config)
